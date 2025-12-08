387827a7acf3dbd6dbc18cf6598008df3c70edcd
Subham Patnaik <patnaiksubham30@gmail.com>
2025-12-08 18:55:04 +0530

    first dsa code push

---
+++ hashmap.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class hashmap {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x=sc.nextInt();
+        System.out.println("ans: " + x);
+     }
+}
