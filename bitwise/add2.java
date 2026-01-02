09813c53274721856cae49542907265ccb2e1652
Subham Patnaik <patnaiksubham30@gmail.com>
2026-01-02 16:21:56 +0530

    bitwise

---
+++ bitwise/add2.java
@@ -0,0 +1,13 @@
+package bitwise;
+import java.util.*;
+public class add2{
+   public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       int x=sc.nextInt();
+       int y=sc.nextInt();
+       x=(x^y);
+       y=(x^y);
+       x=(x^y);
+       System.out.println(x+" ______"+y);
+   }
+}
