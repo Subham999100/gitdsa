d66b84d2baaf236e67e52559f5adeadd84b5980b
Subham999100 <patnaiksubham30@gmail.com>
2026-01-11 21:26:38 +0530

    Time: 3 ms (39.45%) | Memory: 43 MB (75.14%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,17 +1,14 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        HashMap<Character,Integer> freq=new HashMap<>();
+        int xor=0;
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-            freq.put(ch,freq.getOrDefault(ch,0)+1);
+            xor^=ch;
         }
         for(int i=0;i<t.length();i++){
             char ch=t.charAt(i);
-            if(!freq.containsKey(ch)|| freq.get(ch)==0){
-                return ch;
-            }
-            freq.put(ch,freq.get(ch)-1);
+            xor^=ch;
         }
-        return ' ';
+        return (char)xor;
     }
 }
