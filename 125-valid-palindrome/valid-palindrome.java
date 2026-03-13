6dbfd5c5816e27f3313d381100a88073d6752b42
Subham Sekhar Patnaik <patnaiksubham30@gmail.com>
2026-03-13 19:52:06 +0530

    Time: 2 ms (99.14%) | Memory: 44.2 MB (83.61%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -6,19 +6,16 @@ class Solution {
             while(i<j && !Character.isLetterOrDigit(s.charAt(i))){
                 i++;
             }
-
             while(i<j && !Character.isLetterOrDigit(s.charAt(j))){
                 j--;
             }
-            if (Character.toLowerCase(s.charAt(i)) 
-                != Character.toLowerCase(s.charAt(j))) {
+            if(Character.toLowerCase(s.charAt(i))!=Character.toLowerCase(s.charAt(j))){
                 return false;
             }
             i++;
             j--;
-        }
-        return true;
-        
 
+        } 
+        return true;
     }
 }
