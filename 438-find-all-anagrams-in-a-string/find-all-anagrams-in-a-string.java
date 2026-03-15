67edebecaffc78a2e29691af5f19fadc9a592a72
Subham Sekhar Patnaik <patnaiksubham30@gmail.com>
2026-03-15 11:34:39 +0530

    Time: 1834 ms (5.01%) | Memory: 47.8 MB (19.30%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,38 +1,45 @@
+import java.util.*;
+
 class Solution {
-    public boolean check(String kk,String p){
-        int[] freq=new int[26];
-        for(int i=0;i<p.length();i++){
-            char ch=kk.charAt(i);
-            char kh=p.charAt(i);
-            freq[ch-'a']++;
-            freq[kh-'a']--;
-        }
-        for(int n:freq){
-            if(n!=0){
-                return false;
-            }
-        }
-        return true;
+
+    public boolean check(String kk, String sortedP){
+
+        char[] arr = kk.toCharArray();
+        Arrays.sort(arr);
+
+        return new String(arr).equals(sortedP);
     }
+
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> lst=new ArrayList<>();
-        int k=p.length();
-        int i=0;
-        StringBuilder sb=new StringBuilder();
+
+        List<Integer> lst = new ArrayList<>();
+
+        int k = p.length();
+        int i = 0;
+
+        char[] pArr = p.toCharArray();
+        Arrays.sort(pArr);
+        String sortedP = new String(pArr);
+
+        StringBuilder sb = new StringBuilder();
+
         for(int j=0;j<s.length();j++){
-            char ch=s.charAt(j);
-            sb.append(ch);
+
+            sb.append(s.charAt(j));
+
             if(j-i+1>k){
                 sb.deleteCharAt(0);
                 i++;
             }
+
             if(j-i+1==k){
-                String kk=sb.toString();
-                if(check(kk,p)){
+
+                if(check(sb.toString(),sortedP)){
                     lst.add(i);
                 }
             }
         }
+
         return lst;
     }
 }
