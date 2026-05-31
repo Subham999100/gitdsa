6a9aef7564778596e9bf2eb2f5f6ff4936262be9
Subham Sekhar Patnaik <patnaiksubham30@gmail.com>
2026-05-31 17:29:10 +0530

    Time: 18 ms (68.16%) | Memory: 12.4 MB (85.28%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if(len(s)!=len(t)):
+            return False
+        count={}
+        for ch in s:
+            count[ch]=count.get(ch,0)+1
+        for ch in t:
+            count[ch]=count.get(ch,0)-1
+        for v in count.values():
+            if v!=0:
+                return False
+        
+        return True
