a3815721b0a1dd5891f31d5e80442a77b9388b0c
Subham Sekhar Patnaik <patnaiksubham30@gmail.com>
2026-06-03 19:53:27 +0530

    Time: 1 ms (59.88%) | Memory: 12.5 MB (96.77%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        res=[]
+        def dfs(left,right,s):
+            if(left==n and right==n):
+                res.append(s)
+                return
+            if(left<n):
+                dfs(left+1,right,s+'(')
+            if right<left:
+                dfs(left,right+1,s+')')
+        
+        dfs(0,0,'')
+        return res
+    
+        
