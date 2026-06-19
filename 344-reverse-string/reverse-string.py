bf789735bd447e253e78212dd93036bd81dd12d0
Subham Sekhar Patnaik <patnaiksubham30@gmail.com>
2026-06-19 13:24:33 +0530

    Time: 23 ms (5.17%) | Memory: 45 MB (5.38%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -1,14 +1,11 @@
 class Solution(object):
     def reverseString(self, s):
-        """
-        :type s: List[str]
-        :rtype: None Do not return anything, modify s in-place instead.
-        """
-        def solve(start,end):
-            if(start>=end):
-               return
-            temp=s[start]
-            s[start]=s[end]
-            s[end]=temp
-            return solve(start+1,end-1)
-        return solve(0,len(s)-1)
+        def recur(left,right):
+            if left>=right:
+                return
+                
+            s[left],s[right]=s[right],s[left]
+            recur(left+1,right-1)
+        recur(0,len(s)-1)
+            
+        
