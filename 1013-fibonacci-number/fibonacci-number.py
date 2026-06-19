26d34ed949cd772249fc5421927b17a02ece3396
Subham Sekhar Patnaik <patnaiksubham30@gmail.com>
2026-06-19 13:10:49 +0530

    Time: 669 ms (13.60%) | Memory: 12.3 MB (52.25%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -8,5 +8,5 @@ class Solution(object):
             return 0
         if n==1:
             return 1
-        return self.fib(n-1)+self.fib(n-2)
+        return  self.fib(n-1)+self.fib(n-2)
         
