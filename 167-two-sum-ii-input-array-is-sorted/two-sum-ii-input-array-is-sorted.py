e531346dcfa520c2fc847d27a40a65c11c21df08
Subham Sekhar Patnaik <patnaiksubham30@gmail.com>
2026-06-22 14:15:01 +0530

    Time: 5 ms (63.48%) | Memory: 12.9 MB (44.30%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -5,11 +5,14 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        mp={}
-        for i in range(len(numbers)):
-            diff=target-numbers[i]
-            if diff in mp:
-                return [mp[diff]+1,i+1]
+        l=0
+        r=len(numbers)-1
+        while(l<r):
+            s=numbers[l]+numbers[r]
+            if s==target:
+                return [l+1,r+1]
+            elif s<target:
+                l+=1
+            else:
+                r-=1
             
-            mp[numbers[i]]=i
-        
