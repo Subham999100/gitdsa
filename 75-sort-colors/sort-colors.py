2a80d51507e71d395dce6858be077b1b25bdc5ee
Subham Sekhar Patnaik <patnaiksubham30@gmail.com>
2026-08-21 11:35:41 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (91.11%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -7,14 +7,17 @@ class Solution(object):
         low=0
         mid=0
         high=len(nums)-1
-        while mid<=high:
-            if nums[mid]==0:
+        while(mid<=high):
+            if(nums[mid]==0):
                 nums[low],nums[mid]=nums[mid],nums[low]
                 low+=1
                 mid+=1
-            elif nums[mid]==1:
+            elif(nums[mid]==1):
                 mid+=1
             else:
                 nums[high],nums[mid]=nums[mid],nums[high]
                 high-=1
-                
+
+            
+
+        
